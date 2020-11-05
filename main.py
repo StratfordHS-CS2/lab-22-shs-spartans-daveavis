2831c5dead55601d74f51123a33d02e9da72c47e
Dave Avis <charles.avis@springbranchisd.com>
2020-11-05 10:08:42 -0600

    Introduced an error on purpose

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ num = int(input('Enter a number: '))
 for x in range(num + 1):
     if x % 3 == 0 and x % 4 == 0:
         print('SHS Spartans')
-    elif x % 3 == 0:
+    elif x % 2 == 0:
         print('SHS')
     elif x % 4 == 0:
         print('Spartans')
